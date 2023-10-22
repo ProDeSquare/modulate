2e24201aa6eacce9f87102b31f9e29adb7015c9a
Hamza Mughal <hamza@prodesquare.com>
2023-10-22 16:12:39 +0500

    installed `sass`

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/layouts/Modulate";
+import "@/assets/styles/app.scss";
 
 ReactDOM.createRoot(document.getElementById("modulate")!).render(
   <React.StrictMode>
