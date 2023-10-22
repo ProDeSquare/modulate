7c4b309c58f23ffd3c6713d849bd81d1858a7208
Hamza Mughal <hamza@prodesquare.com>
2023-10-22 17:09:07 +0500

    main section

--- src/layouts/Modulate.tsx
+++ src/layouts/Modulate.tsx
@@ -6,7 +6,11 @@ const Modulate = () => (
   <div className="max-w-screen-xl mx-auto font-medium text-gray-800">
     <Header />
 
-    <p>{app.tagline}</p>
+    <main className="px-5">
+      <p>
+        {app.creator.name} - {app.creator.email}
+      </p>
+    </main>
 
     <Footer />
   </div>
