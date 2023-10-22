135ebe738ffc7eaa57b05fa50c026a03ad0b9fbf
Hamza Mughal <hamza@prodesquare.com>
2023-10-22 16:29:38 +0500

    `Footer.tsx` seperated

---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>&copy; {new Date().getFullYear()} Modulate</footer>
+);
+
+export default Footer;
--- src/layouts/Modulate.tsx
+++ src/layouts/Modulate.tsx
@@ -1,10 +1,12 @@
+import Footer from "@/components/sections/Footer";
+
 const Modulate = () => (
   <div className="max-w-screen-xl mx-auto font-medium">
     <h1 className="font-bold text-4xl">Modulate</h1>
 
     <p>Percentages with Precision</p>
 
-    <footer>&copy; {new Date().getFullYear()} Modulate</footer>
+    <Footer />
   </div>
 );
 
