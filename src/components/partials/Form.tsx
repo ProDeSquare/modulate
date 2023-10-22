361e604b61b416bcef58694d9a0f5e5c12835061
Hamza Mughal <hamza@prodesquare.com>
2023-10-23 00:39:46 +0500

    values can be computed

--- src/components/partials/Form.tsx
+++ src/components/partials/Form.tsx
@@ -1,3 +1,10 @@
+import { useState, ChangeEvent, FormEvent } from "react";
+
+type FormFields = {
+  percentile: number;
+  price: number;
+};
+
 const Form = () => {
   const inputClasses: Array<string> = [
     "w-full",
@@ -7,6 +14,56 @@ const Form = () => {
     "border-teal-100",
     "mt-1",
   ];
+
+  const [formData, setFormData] = useState<FormFields>({
+    percentile: 0,
+    price: 0,
+  });
+
+  const [multipleOfTen, setMultipleOfTen] = useState<boolean>(false);
+  const [computedValue, setComputedValue] = useState<number>(0);
+  const [rounded, setRounded] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const formattedValue = (n: number): string =>
+    n % 1 !== 0 ? n.toFixed(2).toLocaleString() : n.toLocaleString();
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+
+    setFormData((prevData: FormFields) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const computeValue = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    if (!formData.percentile || !formData.price || loading) return;
+
+    try {
+      setLoading(true);
+
+      let calculated: number =
+        parseFloat(formData.price.toString()) +
+        (formData.price * formData.percentile) / 100;
+
+      if (rounded) calculated = Math.floor(calculated);
+      if (multipleOfTen) calculated = Math.round(calculated / 10) * 10;
+
+      setComputedValue(calculated);
+
+      setFormData((prevData: FormFields) => ({
+        ...prevData,
+        price: 0,
+      }));
+    } catch {
+      //
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
@@ -22,7 +79,7 @@ const Form = () => {
         </p>
 
         <div>
-          <form action="#">
+          <form action="#" onSubmit={computeValue}>
             <div className="mb-3">
               <label className="text-gray-700" htmlFor="percentile">
                 Percentile (%)
@@ -33,6 +90,8 @@ const Form = () => {
                 placeholder="75"
                 className={inputClasses.join(" ")}
                 id="percentile"
+                name="percentile"
+                onChange={handleInputChange}
               />
             </div>
 
@@ -46,12 +105,20 @@ const Form = () => {
                 placeholder="2390"
                 className={inputClasses.join(" ")}
                 id="price"
+                name="price"
+                onChange={handleInputChange}
+                value={formData.price || ""}
               />
             </div>
 
             <div className="flex gap-4">
               <div className="mb-3 flex gap-2">
-                <input type="checkbox" id="multiple" />
+                <input
+                  type="checkbox"
+                  id="multiple"
+                  name="multiple_of_10"
+                  onChange={() => setMultipleOfTen(!multipleOfTen)}
+                />
 
                 <label className="text-gray-700" htmlFor="multiple">
                   Multiple of 10
@@ -59,7 +126,12 @@ const Form = () => {
               </div>
 
               <div className="mb-3 flex gap-2">
-                <input type="checkbox" id="round" />
+                <input
+                  type="checkbox"
+                  id="round"
+                  name="rounded"
+                  onChange={() => setRounded(!rounded)}
+                />
 
                 <label className="text-gray-700" htmlFor="round">
                   Round figure
@@ -72,7 +144,7 @@ const Form = () => {
                 className="bg-gray-900 text-white px-6 py-3 hover:bg-gray-700 transition-colors ease-in"
                 type="submit"
               >
-                Compute
+                {loading ? "Computing" : "Compute"}
                 <span className="text-teal-500 font-bold">.</span>
               </button>
             </div>
@@ -87,7 +159,9 @@ const Form = () => {
         </h3>
 
         <p className="text-6xl flex tracking-tight mt-2">
-          <span className="text-gray-700 truncate">--</span>
+          <span className="text-gray-700 truncate">
+            {computedValue > 0 ? formattedValue(computedValue) : "--"}
+          </span>
           <span className="text-gray-200 select-none">/-</span>
         </p>
       </section>
