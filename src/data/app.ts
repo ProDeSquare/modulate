d2d3d430e2fa478b12eeaab8f79207a6baa221dd
Hamza Mughal <hamza@prodesquare.com>
2023-10-22 16:37:46 +0500

    app data

---
+++ src/data/app.ts
@@ -0,0 +1,13 @@
+const app = {
+  title: "Modulate",
+  tagline: "Percentages with Precision",
+  initYear: 2023,
+  creator: {
+    name: "Hamza Mughal",
+    title: "ProDeSquare",
+    email: "hamza@prodesquare.com",
+    website: "https://prodesquare.com",
+  },
+};
+
+export default app;
--- src/layouts/Modulate.tsx
+++ src/layouts/Modulate.tsx
@@ -1,10 +1,11 @@
 import Footer from "@/components/sections/Footer";
+import app from "@/data/app";
 
 const Modulate = () => (
-  <div className="max-w-screen-xl mx-auto font-medium">
-    <h1 className="font-bold text-4xl">Modulate</h1>
+  <div className="max-w-screen-xl mx-auto font-medium text-gray-800">
+    <h1 className="font-bold text-4xl">{app.title}.</h1>
 
-    <p>Percentages with Precision</p>
+    <p>{app.tagline}</p>
 
     <Footer />
   </div>
