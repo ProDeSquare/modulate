f2df559592fae22699cdd5f4deab10ec4771131d
Hamza Mughal <hamza@prodesquare.com>
2023-10-29 01:26:10 +0500

    form moved inside container

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -96,88 +96,91 @@ const Home = () => {
 
   return (
     <>
-      <section className="p-5 bg-teal-50 flex-1 md:w-6/12">
-        <h3 className="text-2xl md:text-3xl font-bold border-b border-teal-100 md:border-none pb-1 md:pb-0">
-          Calculate
-        </h3>
+      <section className="flex-1 md:w-6/12">
+        <div className="p-5 bg-teal-50">
+          <h3 className="text-2xl md:text-3xl font-bold border-b border-teal-100 md:border-none pb-1 md:pb-0">
+            Calculate
+            <span className="text-teal-500">.</span>
+          </h3>
 
-        <p className="my-3 leading-relaxed">
-          Fill in the form below to compute the adjusted value after adding or
-          subtracting a percentage from your number. Get precise results
-          instantly!
-        </p>
+          <p className="my-3 leading-relaxed">
+            Fill in the form below to compute the adjusted value after adding or
+            subtracting a percentage from your number. Get precise results
+            instantly!
+          </p>
 
-        <div>
-          <form action="#" onSubmit={computeValue}>
-            <div className="mb-3">
-              <label className="text-gray-700" htmlFor="percentile">
-                Percentile (%)
-              </label>
-
-              <input
-                type="number"
-                placeholder="75"
-                className={inputClasses.join(" ")}
-                id="percentile"
-                name="percentile"
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div className="mb-3">
-              <label className="text-gray-700" htmlFor="price">
-                Price/Value
-              </label>
-
-              <input
-                type="number"
-                placeholder="2390"
-                className={inputClasses.join(" ")}
-                id="price"
-                name="price"
-                onChange={handleInputChange}
-                value={formData.price || ""}
-              />
-            </div>
-
-            <div className="flex gap-4">
-              <div className="mb-3 flex gap-2">
-                <input
-                  type="checkbox"
-                  id="multiple"
-                  name="multiple_of_10"
-                  onChange={() => setMultipleOfTen(!multipleOfTen)}
-                />
-
-                <label className="text-gray-700" htmlFor="multiple">
-                  Multiple of 10
+          <div>
+            <form action="#" onSubmit={computeValue}>
+              <div className="mb-3">
+                <label className="text-gray-700" htmlFor="percentile">
+                  Percentile (%)
                 </label>
+
+                <input
+                  type="number"
+                  placeholder="75"
+                  className={inputClasses.join(" ")}
+                  id="percentile"
+                  name="percentile"
+                  onChange={handleInputChange}
+                />
               </div>
 
-              <div className="mb-3 flex gap-2">
-                <input
-                  type="checkbox"
-                  id="round"
-                  name="rounded"
-                  onChange={() => setRounded(!rounded)}
-                />
-
-                <label className="text-gray-700" htmlFor="round">
-                  Round figure
+              <div className="mb-3">
+                <label className="text-gray-700" htmlFor="price">
+                  Price/Value
                 </label>
-              </div>
-            </div>
 
-            <div className="mt-4">
-              <button
-                className="bg-gray-900 text-white px-6 py-3 hover:bg-gray-700 transition-colors ease-in"
-                type="submit"
-              >
-                {loading ? "Computing" : "Compute"}
-                <span className="text-teal-500 font-bold">.</span>
-              </button>
-            </div>
-          </form>
+                <input
+                  type="number"
+                  placeholder="2390"
+                  className={inputClasses.join(" ")}
+                  id="price"
+                  name="price"
+                  onChange={handleInputChange}
+                  value={formData.price || ""}
+                />
+              </div>
+
+              <div className="flex gap-4">
+                <div className="mb-3 flex gap-2">
+                  <input
+                    type="checkbox"
+                    id="multiple"
+                    name="multiple_of_10"
+                    onChange={() => setMultipleOfTen(!multipleOfTen)}
+                  />
+
+                  <label className="text-gray-700" htmlFor="multiple">
+                    Multiple of 10
+                  </label>
+                </div>
+
+                <div className="mb-3 flex gap-2">
+                  <input
+                    type="checkbox"
+                    id="round"
+                    name="rounded"
+                    onChange={() => setRounded(!rounded)}
+                  />
+
+                  <label className="text-gray-700" htmlFor="round">
+                    Round figure
+                  </label>
+                </div>
+              </div>
+
+              <div className="mt-4">
+                <button
+                  className="bg-gray-900 text-white px-6 py-3 hover:bg-gray-700 transition-colors ease-in"
+                  type="submit"
+                >
+                  {loading ? "Computing" : "Compute"}
+                  <span className="text-teal-500 font-bold">.</span>
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </section>
 
