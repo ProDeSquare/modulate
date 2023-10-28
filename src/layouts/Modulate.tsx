603db34fdbf401eb5b5b70713a6ea5d2d58c083d
Hamza Mughal <hamza@prodesquare.com>
2023-10-29 00:21:25 +0500

    fixed `Home.tsx` import

--- src/layouts/Modulate.tsx
+++ src/layouts/Modulate.tsx
@@ -1,4 +1,4 @@
-import Home from "@/components/partials/Home";
+import Home from "@/components/pages/Home";
 import Header from "@/components/sections/Header";
 import Footer from "@/components/sections/Footer";
 
