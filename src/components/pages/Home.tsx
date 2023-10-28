2e2b017fcb1c0cfc7cbdb9270ec71836fd89a59a
Hamza Mughal <hamza@prodesquare.com>
2023-10-29 00:51:48 +0500

    seperated `History.tsx` component

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useState, ChangeEvent, FormEvent } from "react";
+import History from "@/components/partials/History";
 import Result from "@/components/partials/Result";
 
 type FormFields = {
@@ -195,37 +196,14 @@ const Home = () => {
 
         {history.length > 0 && (
           <section className="mt-5">
-            <div className="border-t border-teal-100 md:w-8/12"></div>
+            <History history={history} />
 
-            <h3 className="font-bold mt-3 md:mt-2 text-3xl md:text-4xl leading-8">
-              Previous Computations<span className="text-teal-500">.</span>
-            </h3>
-
-            <div>
-              <ul className="mt-3 flex flex-wrap text-teal-700 -mx-1">
-                {history.map((computation: ComputedValues, index: number) => (
-                  <li className="w-6/12" key={index}>
-                    <ul className="bg-teal-100 border hover:bg-teal-50 border-teal-200 transition-colors duration-300 rounded m-1 px-3 py-2 flex flex-col tracking-tight md:tracking-normal">
-                      <ul className="flex justify-between text-sm gap-1">
-                        <li className="truncate">{computation.price}/-</li>
-                        <li className="font-bold">{computation.percentile}%</li>
-                      </ul>
-
-                      <li className="text-xl font-bold mt-1 text-teal-800 truncate">
-                        {formattedValue(computation.computed)}
-                      </li>
-                    </ul>
-                  </li>
-                ))}
-              </ul>
-
-              <button
-                className="text-red-900 bg-red-200 hover:bg-red-100 px-4 py-1 mt-3 rounded border border-red-300 transition-colors duration-300"
-                onClick={clearHistory}
-              >
-                Clear
-              </button>
-            </div>
+            <button
+              className="text-red-900 bg-red-200 hover:bg-red-100 px-4 py-1 mt-3 rounded border border-red-300 transition-colors duration-300"
+              onClick={clearHistory}
+            >
+              Clear
+            </button>
           </section>
         )}
       </div>
---
+++ src/components/partials/History.tsx
@@ -0,0 +1,34 @@
+type HistoryArgs = "price" | "percentile" | "computed";
+
+type HistoryType = Record<HistoryArgs, number>;
+
+const History = ({ history }: { history: HistoryType[] }) => (
+  <>
+    <div className="border-t border-teal-100 md:w-8/12"></div>
+
+    <h3 className="font-bold mt-3 md:mt-2 text-3xl md:text-4xl leading-8">
+      Previous Computations<span className="text-teal-500">.</span>
+    </h3>
+
+    <div>
+      <ul className="mt-3 flex flex-wrap text-teal-700 -mx-1">
+        {history.map((computation: HistoryType, index: number) => (
+          <li className="w-6/12" key={index}>
+            <ul className="bg-teal-100 border hover:bg-teal-50 border-teal-200 transition-colors duration-300 rounded m-1 px-3 py-2 flex flex-col tracking-tight md:tracking-normal">
+              <ul className="flex justify-between text-sm gap-1">
+                <li className="truncate">{computation.price}/-</li>
+                <li className="font-bold">{computation.percentile}%</li>
+              </ul>
+
+              <li className="text-xl font-bold mt-1 text-teal-800 truncate">
+                {computation.computed}
+              </li>
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </>
+);
+
+export default History;
